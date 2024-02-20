4e6a9d445c989aff58f75174aaeb99000e9a5537
shasidhar1986 <kanth.uma1986@gmail.com>
2024-02-20 13:36:06 +0530

    new files added
    new files

---
+++ src/main/java/com/demo/sharak/SharakApplication.java
@@ -0,0 +1,13 @@
+package com.demo.sharak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharakApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/sharak/SharakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.sharak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
